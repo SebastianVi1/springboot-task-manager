310ced89331e1c80319e18ba9ecd8764a256e1b4
Andre <sebastianvh86@gmail.com>
2025-07-06 22:42:52 -0600

    task model

---
+++ src/main/java/org/sebas/taskmanager/model/Task.java
@@ -0,0 +1,28 @@
+package org.sebas.taskmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private LocalDateTime createdAt;
+
+
+}
